bd6c25ed9e7f3b6c398cc1a5cead030c972e2015
EasyT-T <116633144+EasyT-T@users.noreply.github.com>
2026-08-15 12:28:24 +0800

    Fix wrong type name. Remove register_method Add HotReload support.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ mod common;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn load_ccb() -> bool{
+    unsafe { std::env::set_var("DOTNET_MODIFIABLE_ASSEMBLIES", "debug"); }
+
     let hostfxr = match netcorehost::nethost::load_hostfxr(){
         Ok(hostfxr) => hostfxr,
         Err(e) => {
@@ -29,7 +31,7 @@ pub extern "C" fn load_ccb() -> bool{
     };
     
     let function = fn_loader.get_function_with_unmanaged_callers_only::<fn()>(
-        netcorehost::pdcstr!("CCB.Internal.Interop"),
+        netcorehost::pdcstr!("CCB.Internal.Interop, CCB"),
         netcorehost::pdcstr!("Load"))
         .expect("Unable to invoke CCB::Internal::Interop::Load()");
     
@@ -38,16 +40,4 @@ pub extern "C" fn load_ccb() -> bool{
     common::CLR_FUNCTION_LOADER.set(fn_loader).unwrap_or_else(|_| panic!("Unable to set the CLR_FUNCTION_LOADER"));
     
     true
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn register_method(index: i32, class_name_ptr: *const c_char, method_name_ptr: *const c_char, func_ptr: *const c_void){
-    let function_loader = common::CLR_FUNCTION_LOADER.get().unwrap_or_else(|| panic!("Unable to get the CLR_FUNCTION_LOADER"));
-
-    let function = function_loader.get_function_with_unmanaged_callers_only::<fn(i32, *const c_char, *const c_char, *const c_void)>(
-        netcorehost::pdcstr!("CCB.Internal.Interop, CCB"),
-        netcorehost::pdcstr!("RegisterMethod"))
-        .expect("Unable to register method");
-
-    function(index, class_name_ptr, method_name_ptr, func_ptr);
 }
